d03d3033a482b41fac2736f1886631ee96ea75b5
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-07 00:38:27 -0300

    :sparkles: feat: Add dynamic data from stripe's API

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  images: {
+    domains: ["files.stripe.com"]
+  },
+
   experimental: {
     images: {
       allowFutureImage: true,
