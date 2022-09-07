24ae062fa3952e7886c9abb44e4812a7faabbd96
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-07 02:00:55 -0300

    :sparkles: feat: Create Checkout for product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { HomeContainer, Product } from "../styles/pages/home"
 
 import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from "next"
+import Link from 'next/link'
 import { Stripe } from 'stripe'
 import { stripe } from "../lib/stripe"
 
@@ -12,7 +13,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -27,14 +28,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => (
-        <Product key={product.id} className="keen-slider__slide">
-        <Image src={product.imageUrl} width={520} height={480} alt="" />
+       <Link href={`/product/${product.id}`} key={product.id} passHref prefetch={false}>
+         <Product  className="keen-slider__slide">
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-        <footer>
-          <strong>{product.name}</strong>
-          <span>{product.price}</span>
-        </footer>
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer> 
         </Product>
+       </Link>
       ))}
     </HomeContainer>
   )
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,99 @@
+import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from "next"
+import Image from "next/future/image"
 import { useRouter } from "next/router"
+import { useState } from 'react'
+import { Stripe } from 'stripe'
+import { stripe } from "../../lib/stripe"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+    defaultPriceId: string
+  }
+}
 
-export default function Product() {
-  const { query } = useRouter()
-  return <h1>Product Page</h1>
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false)
+      alert('Falha ao redirecionar ao checkout')
+    }
+  }
+
+  const { isFallback } = useRouter()
+
+  if(isFallback) {
+    return <p>Loading...</p>
+  }
+
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt=""/>
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar agora</button>
+
+      </ProductDetails>
+    </ProductContainer>
+  )  
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: "prod_MNzPUFSjc0sWAF" } }
+    ],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({params}) => {
+  const { id } = params;
+  
+  const product = await stripe.products.retrieve(id, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0] ?? "",
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+        defaultPriceId: price.id,
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
 }
